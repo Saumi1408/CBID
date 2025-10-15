8fe60839c678fe611afc44a7bd3085262ecc50d9
Nithiish S.D <nithiishdanasekar@gmail.com>
2025-10-15 19:05:00 +0530

    changes in firebase

--- firebaseconfig.tsx
+++ firebaseconfig.tsx
@@ -1,14 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "./node_modules/firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAy1y0MNUxHjtlhGWe3C8uTI5NHNtt7RHM",
   authDomain: "codebid-4fe06.firebaseapp.com",
